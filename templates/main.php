44974d58828e1dc5048cd2ffd9d39e9bf99b0d44
Phrlog <shiftiam@yandex.ru>
2016-08-10 23:12:04 +0300

    add bootstrap, improve student list

--- templates/main.php
+++ templates/main.php
@@ -4,10 +4,10 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Тайтл</title>
-	<link rel="stylesheet" href="">
+	<link href="/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="/css/main.css">
 </head>
 <body>
-	<p>Hello</p>
 	<?php include $content ?>
 </body>
 </html>
