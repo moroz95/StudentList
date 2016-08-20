4fad3d792e38d18ea17aadd677468a7c0aac36cb
Phrlog <shiftiam@yandex.ru>
2016-08-20 03:10:53 +0300

    improve validation

--- app/Validation.php
+++ app/Validation.php
@@ -26,90 +26,99 @@ class Validation
         $this->validateBirthDate($student->birthDate);
         $this->validateSexOpt($student->sex);
         $this->validateLocation($student->location);
+    }
 
+    public function getErrors()
+    {
         return $this->errors;
+    }
+
+    public function hasError($name)
+    {
+        return key_exists($name, $this->errors);
+    }
+
+    private function applyTags($str)
+    {
+        return "<div class='alert alert-danger' role='alert'>$str</div>";
     }
 
     public function validateFirstName($name)
     {
         if (!$name) {
-            $this->errors["firstName"] = "Не заполнено поле ввода имени.";
+            $this->errors["firstName"] = $this->applyTags("Не заполнено поле ввода имени");
         } elseif (mb_strlen($name) > 200) {
-            $this->errors["firstName"] = "Имя не должно превышать 200 символов! Вы ввели " . mb_strlen($name) . ".";
+            $this->errors["firstName"] = $this->applyTags("Имя не должно превышать 200 символов! Вы ввели " . (int)mb_strlen($name));
         }
         if (!preg_match("/[а-яА-Я'\-]/u", $name)) {
-            $this->errors["firstName"] .= "Неверный формат имени.";
+            $this->errors["firstName"] .= $this->applyTags("Неверный формат имени");
         }
     }
 
     public function validateLastName($name)
     {
         if (!$name) {
-            $this->errors["lastName"] = "Не заполнено поле ввода фамилии.";
+            $this->errors["lastName"] = $this->applyTags("Не заполнено поле ввода фамилии");
         } elseif (mb_strlen($name) > 200) {
-            $this->errors["lastName"] = "Фамилия не должна превышать 200 символов. Вы ввели " . mb_strlen($name) . ".";
+            $this->errors["lastName"] = $this->applyTags("Фамилия не должна превышать 200 символов. Вы ввели " . (int)mb_strlen($name));
         }
         if (!preg_match("/[а-яА-Я'\-]/u", $name)) {
-            $this->errors["lastName"] .= "Неверный формат фамилии.";
+            $this->errors["lastName"] .= $this->applyTags("Неверный формат фамилии");
         }
     }
 
     public function validateGroupNumber($group)
     {
         if (mb_strlen($group) > 5) {
-            $this->errors["groupNumber"] = "Номер группы не должен превышать 5 символов! Вы ввели " . mb_strlen($group) . ".";
+            $this->errors["groupNumber"] = $this->applyTags("Номер группы не должен превышать 5 символов, а вы ввели " . (int)mb_strlen($group));
         } elseif (mb_strlen($group) < 2) {
-            $this->errors["groupNumber"] = "Номер группы должен быть более 2 символов! Вы ввели " . mb_strlen($group) . ".";
+            $this->errors["groupNumber"] = $this->applyTags("Номер группы должен быть более 2 символов, а вы ввели " . (int)mb_strlen($group));
         }
         if (!preg_match("/[а-яА-Я0-9]/u", $group)) {
-            $this->errors["groupNumber"] .= "Недопустимый формат номера группы.
-                                           Разрешается использовать только буквы или цифры.";
+            $this->errors["groupNumber"] .= $this->applyTags("Недопустимый формат номера группы, разрешается использовать только буквы или цифры");
         }
     }
 
     public function validateEmail($email)
     {
         if (!$email) {
-            $this->errors["email"] = "Не заполнено поле воода email.";
+            $this->errors["email"] = $this->applyTags("Не заполнено поле воода email");
         } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $this->errors["email"] = "Неверный формат email адреса.";
+            $this->errors["email"] = $this->applyTags("Неверный формат email адреса");
         } elseif (!$this->dataGateway->isUniqueEmail($email)) {
-            $this->errors["email"] = "Вы уже зарегистрированы.";
+            $this->errors["email"] = $this->applyTags("Вы уже зарегистрированы");
         }
-
     }
 
     public function validateMark($mark)
     {
         if (!$mark) {
-            $this->errors["mark"] = "Не заполнено поле воода балла ЕГЭ.";
-        }
-        if ($mark < 100 or $mark > 300) {
-            $this->errors["mark"] = "Балл ЕГЭ должен быть в диапазоне от 100 до 300.
-                                     Вы ввели {$mark}";
+            $this->errors["mark"] = $this->applyTags("Не заполнено поле ввода балла ЕГЭ");
+        } else if ($mark < 100 or $mark > 300) {
+            $this->errors["mark"] = $this->applyTags("Балл ЕГЭ должен быть в диапазоне от 100 до 300, а вы ввели {$mark}");
         }
     }
 
     public function validateBirthDate($year)
     {
         if (!$year) {
-            $this->errors["birthDate"] = "Не заполнено поле ввода года рождения.";
+            $this->errors["birthDate"] = $this->applyTags("Не заполнено поле ввода года рождения");
         } elseif (intval($year) < 1930 or intval($year) > date("Y") - 10) {
-            $this->errors["birthDate"] = "Вы не можете поступить в наш университет по возрастной категории.";
+            $this->errors["birthDate"] = $this->applyTags("Вы не можете поступить в наш университет по возрастной категории");
         }
     }
 
     public function validateSexOpt($opt)
     {
         if (!$opt) {
-            $this->errors["sex"] = "Не выбран пол.";
+            $this->errors["sex"] = $this->applyTags("Не выбран пол");
         }
     }
 
     public function validateLocation($location)
     {
         if (!$location) {
-            $this->errors["location"] = "Укажите, местный вы или нет.";
+            $this->errors["location"] = $this->applyTags("Укажите, местный вы или нет");
         }
     }
 
