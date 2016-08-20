ec31f16b6e958b7b0d5c5a1da4985ac537098e03
Phrlog <shiftiam@yandex.ru>
2016-08-20 03:07:19 +0300

    new class view

---
+++ app/View.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Class View
+ */
+class View {
+    const VIEWS_BASEDIR = "../templates/";
+    /**
+     * @param $template
+     * @param array $params
+     * @return string
+     */
+    protected function fetchPartial($template, $params = array()){
+        extract($params);
+        ob_start();
+        include self::VIEWS_BASEDIR.$template.'.php';
+        return ob_get_clean();
+    }
+    /**
+     * @param $template
+     * @param array $params
+     */
+    public function renderPartial($template, $params = array()){
+        echo $this->fetchPartial($template, $params);
+    }
+    /**
+     * @param $template
+     * @param array $params
+     * @return string
+     */
+    protected function fetch($template, $params = array()){
+        $content = $this->fetchPartial($template, $params);
+        return $this->fetchPartial('main', array('content' => $content));
+    }
+    /**
+     * @param $template
+     * @param array $params
+     */
+    public function render($template, $params = array()){
+        echo $this->fetch($template, $params);
+    }
+}
