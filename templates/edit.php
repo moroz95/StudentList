e0d0cb581ae77bfb3662d9828e6f23e4fb3b5b3d
Phrlog <shiftiam@yandex.ru>
2016-08-20 03:11:33 +0300

    improve templates and view

--- templates/edit.php
+++ templates/edit.php
@@ -1,41 +1,14 @@
-<form action="/<?= $page ?>" method="post" class="form-horizontal">
-    <div class="form-group">
-        <label class="control-label col-xs-3" for="lastName">Фамилия:</label>
+<form action="/<?= $page ?>" method="post" class="form-horizontal ">
+    <?php foreach ($form as $key => $value): ?>
+    <div class="form-group <?php if($validate) $validate->hasError($key) and print "has-error";?>">
+        <label class="control-label col-xs-3" for="<?= $key ?>"><?= $value[0]?></label>
         <div class="col-xs-9">
-            <input type="text" class="form-control" id="lastName" name="lastName" placeholder="Введите фамилию">
+            <input type="<?= $value[2]?>" class="form-control" id="<?= $key ?>" name="<?= $key ?>" placeholder="<?= $value[1]?>">
+            <?php if(!empty($value[3])) print $value[3];?>
         </div>
     </div>
-    <div class="form-group">
-        <label class="control-label col-xs-3" for="firstName">Имя:</label>
-        <div class="col-xs-9">
-            <input type="text" class="form-control" id="firstName" name="firstName" placeholder="Введите имя">
-        </div>
-    </div>
-    <div class="form-group">
-        <label class="control-label col-xs-3"">Баллы за ЕГЭ:</label>
-        <div class="col-xs-9">
-            <input type="number" class="form-control" id="mark" name="mark"">
-        </div>
-    </div>
-    <div class="form-group">
-        <label class="control-label col-xs-3">Дата рождения:</label>
-        <div class="col-xs-9">
-            <input id="age" type="date" min="1920-01-01" max="2000-01-01" name="birthDate">
-        </div>
-    </div>
-    <div class="form-group">
-        <label class="control-label col-xs-3"">Номер группы:</label>
-        <div class="col-xs-9">
-            <input type="text" class="form-control" id="groupNumber" placeholder="Номер группы" name="groupNumber">
-        </div>
-    </div>
-    <div class="form-group">
-        <label class="control-label col-xs-3" for="inputEmail">Email:</label>
-        <div class="col-xs-9">
-            <input type="email" class="form-control" id="inputEmail" placeholder="Email" name="email">
-        </div>
-    </div>
-    <div class="form-group">
+    <?php endforeach;?>
+    <div class="form-group <?php if($validate) $validate->hasError("sex") and print "has-error";?>">
         <label class="control-label col-xs-3">Пол:</label>
         <div class="col-xs-2">
             <label class="radio-inline">
@@ -48,8 +21,8 @@
             </label>
         </div>
     </div>
-    <div class="form-group">
-        <label class="control-label col-xs-3">Место жительства:</label>
+    <div class="form-group <?php if($validate) $validate->hasError("location") and print "has-error";?>">
+        <label class="control-label col-xs-3" >Место жительства:</label>
         <div class="col-xs-2">
             <label class="radio-inline">
                 <input type="radio" name="location" value="local"> Местный
