6143c5443a73e543773b290fdba26ea53d83bef4
Phrlog <shiftiam@yandex.ru>
2016-08-23 17:40:54 +0300

    page variable in fetch method

--- app/View.php
+++ app/View.php
@@ -29,7 +29,7 @@ class View {
      */
     protected function fetch($template, $params = array()){
         $content = $this->fetchPartial($template, $params);
-        return $this->fetchPartial('main', array('content' => $content));
+        return $this->fetchPartial('main', array('content' => $content, 'page' => $params['page']));
     }
     /**
      * @param $template
