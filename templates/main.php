bc301a4ae96d2cce38f3f27eaed9ba54cc44399a
Phrlog <shiftiam@yandex.ru>
2016-08-23 17:41:32 +0300

    add nav tabs

--- templates/main.php
+++ templates/main.php
@@ -8,6 +8,11 @@
 	<link rel="stylesheet" href="/css/main.css">
 </head>
 <body>
+<ul class="nav nav-tabs">
+	<li role="presentation" <?php $page == 'students' and print "class='active'"?>><a href="/index">Студенты</a></li>
+	<li role="presentation" <?php $page == 'edit' and print "class='active'"?>><a href="/edit">Изменение информации</a></li>
+	<li role="presentation" <?php $page == 'register' and print "class='active'"?>><a href="/register">Регистрация</a></li>
+</ul>
 	<?= $content ?>
 </body>
 </html>
