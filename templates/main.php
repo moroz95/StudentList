b6cad98f4c93660f0f7b633872e989a2c6c69915
Phrlog <shiftiam@yandex.ru>
2016-09-05 01:06:19 +0300

    clear template variable name and bug fix

--- templates/main.php
+++ templates/main.php
@@ -3,17 +3,17 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title><?= $page ?></title>
+    <title><?= $url_template ?></title>
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/css/main.css">
 </head>
 <body>
 <ul class="nav nav-tabs">
-    <li role="presentation" <?php $page == 'index' and print "class='active'" ?>><a href="/index">Студенты</a></li>
+    <li role="presentation" <?php (($url_template == 'index') OR ($url_template == 'search')) and print "class='active'" ?>><a href="/index">Студенты</a></li>
     <?php if (isset($_COOKIE['password'])): ?>
-        <li role="presentation" <?php $page == 'edit' and print "class='active'" ?>><a href="/edit">Изменение информации</a></li>
+        <li role="presentation" <?php $url_template == 'edit' and print "class='active'" ?>><a href="/edit">Изменение информации</a></li>
     <?php else: ?>
-        <li role="presentation" <?php $page == 'register' and print "class='active'" ?>><a href="/register">Регистрация</a></li>
+        <li role="presentation" <?php $url_template == 'register' and print "class='active'" ?>><a href="/register">Регистрация</a></li>
     <?php endif; ?>
 </ul>
 <?= $content ?>
