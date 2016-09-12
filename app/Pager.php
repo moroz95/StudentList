ff360d780794bd45ff50ed3bb4c06a8a36c35aed
Phrlog <shiftiam@yandex.ru>
2016-09-12 20:20:35 +0300

    add nextPage and lastPage methods

--- app/Pager.php
+++ app/Pager.php
@@ -9,15 +9,15 @@
  */
 class Pager
 {
-    public  $totalPages;
+    public  $totalPages = null;
     public  $recordsPerPage;
     private $model;
 
     /**
      * Pager constructor.
      *
-     * @param StudentDataGateway $model          model injection
-     * @param integer            $recordsPerPage student per page
+     * @param StudentDataGateway $model model injection
+     * @param integer $recordsPerPage student per page
      */
     public function __construct(StudentDataGateway $model, $recordsPerPage = 5)
     {
@@ -33,19 +33,47 @@ class Pager
     public function getTotalPages($search = '')
     {
         $number_of_students = $this->model->countStudents($search);
-        $pages = ceil($number_of_students/$this->recordsPerPage);
-        return $pages;
+        $this->totalPages = ceil($number_of_students / $this->recordsPerPage);
+        return $this->totalPages;
     }
 
     /**
      * @param string $template page to build
-     * @param array  $params   parameters
+     * @param array $params parameters
      *
      * @return string          link
      */
     public function buildLink($template, $params)
     {
-        $link = htmlspecialchars("$template/?" . http_build_query($params));
+        $link = htmlspecialchars("/$template/?" . http_build_query($params));
         return $link;
+    }
+
+    public function lastPage($template, $params, $current_page)
+    {
+        if ($this->totalPages === null) {
+            $this->totalPages = $this->getTotalPages();
+        }
+
+        if ($current_page <= 1) {
+            return null;
+        } else {
+            $params['page'] = $current_page - 1;
+            return $this->buildLink($template, $params);
+        }
+    }
+
+    public function nextPage($template, $params, $current_page)
+    {
+        if ($this->totalPages === null) {
+            $this->totalPages = $this->getTotalPages();
+        }
+
+        if ($current_page >= $this->totalPages) {
+            return null;
+        } else {
+            $params['page'] = $current_page + 1;
+            return $this->buildLink($template, $params);
+        }
     }
 }
